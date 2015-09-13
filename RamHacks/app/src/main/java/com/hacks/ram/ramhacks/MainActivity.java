ac29f324e0f346c6581fcab66aaa1c50670c5d11
adel <shadel.shsh@gmail.com>
2015-09-12 23:41:47 -0400

    Merge branch 'feature/login' into Feature/TweeterFeed
    Conflicts:
    	RamHacks/app/build.gradle
    	RamHacks/app/src/main/AndroidManifest.xml
    	RamHacks/app/src/main/res/values/strings.xml

--- RamHacks/app/src/main/java/com/hacks/ram/ramhacks/MainActivity.java
+++ RamHacks/app/src/main/java/com/hacks/ram/ramhacks/MainActivity.java
@@ -1,17 +1,61 @@
 package com.hacks.ram.ramhacks;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    LoginButton loginBtn;
+    CallbackManager callbackManager;
+    TextView fbMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Context context = this;
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        loginBtn = (LoginButton) findViewById(R.id.login_button);
+        loginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(context,TwitterActivity.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancel() {
+                fbMessage.setText("Your Request Has Been Canceled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                fbMessage.setText("Sorry Invalid Username or Password");
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
